8a20ec449433838c1e54728016347722937f7dff
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-07-06 00:27:30 +0700

    memperbaiki arr[j + 1] = arr[j];

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -37,7 +37,7 @@ void insertionSort() {
         j = i - 1;
 
         while (j >= 0 && arr[j] > temp) {       //4. looping while dimana nilai j lebih besar sama dengan 0 dan array[j] lebih besar dari temp
-            arr[j + 1] - arr[j];                //simpan arr[j] kedalam variable arr[j+1]
+            arr[j + 1] = arr[j];                //simpan arr[j] kedalam variable arr[j+1]
             j--;                                //j decrement
         }
 
